465d54f8bdddbbd97d408de7ee277ba8851f82ca
imdhruv99 <dpdhruvprajapati@gmail.com>
2021-09-07 16:27:59 +0530

    [ADDED] Firebase and Completed functionality

--- shopping-cart-advanced-redux/src/components/Shop/ProductItem.js
+++ shopping-cart-advanced-redux/src/components/Shop/ProductItem.js
@@ -1,15 +1,48 @@
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
-import { useDispatch } from 'react-redux';
+import { useDispatch  } from 'react-redux';
 import { cartActions } from '../../store/cart-slice';
 
 const ProductItem = (props) => {
+
   
   const dispatch = useDispatch();
 
   const { title, price, description, id } = props;
-
+  
+  //const cart = useSelector((state) => state.cart);
   const addToCartHandler = () => {
+
+    // const newTotalQuantity = cart.totalQuantity + 1
+    // // creating copy via slice to avoid mutating the original state
+    // const updatedItems = cart.items.slice();
+
+    // const existingItem = updatedItems.find((item) => item.id === id);
+
+    // if (existingItem) {
+    //   const updatedItem = { ...existingItem }; // new object + copy existing properties to avoid state mutation
+    //   updatedItem.quantity++;
+    //   updatedItem.totalPrice = updatedItem.totalPrice + price;
+    //   const existingItemIndex = updatedItems.findIndex(
+    //     (item) => item.id === id
+    //   );
+    //   updatedItems[existingItemIndex] = updatedItem;
+    // } else {
+    //   updatedItems.push({
+    //     id: id,
+    //     price: price,
+    //     quantity: 1,
+    //     totalPrice: price,
+    //     name: title,
+    //   });
+    // }
+
+    // const newCart = {
+    //   totalQuantity: newTotalQuantity,
+    //   items: updatedItems,
+    // };
+
+    //dispatch(cartActions.replaceCart(newCart));
     dispatch(
       cartActions.addItemToCart({
         id, 
